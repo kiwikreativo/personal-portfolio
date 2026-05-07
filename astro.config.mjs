394fdc6dbf09e2e9edf060b38f2a0dfdab20191f
Kiwi <guillermoj.obando@gmail.com>
2026-05-07 11:14:24 -0600

    Corrected the icons and name from the skills set installed netlify dependencie

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: netlify(),
 });
